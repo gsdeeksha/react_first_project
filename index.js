2ee3b5c80c1f7f44184e97ca9684d06fe4ac880f
gsdeeksha <deekshags18@gmail.com>
2020-10-16 11:37:00 +0530

    commit message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app= express()
-const port= 4000
+const port= process.env.port||4000
 const cors= require('cors')
 const routes = require('./route/note.route')
 
@@ -14,14 +14,16 @@ app.use(bodyParser.urlencoded({extended:true}))
 const Dbconf = require('./config/database.config')
 const mongoose = require('mongoose')
 
-mongoose.connect(Dbconf.url, {useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.MONGODB_URI || Dbconf.url, {useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
 console.log('connected to db successfully')
 })
 .catch(()=>{
     console.log('unable to connect to db ')    
 })
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
 
-
+}
 app.use('/notes',routes)
 app.listen(port, ()=>{console.log('listening on '+port)})
